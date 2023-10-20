39b2a697b37f7e8150f59fcc61ce5bf8fbe504f6
jiyeon11 <leejiyeon415@gmail.com>
2023-10-21 07:14:01 +0900

    second commit

---
+++ app/src/main/java/com/cookandroid/noti/GameInfoActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.noti;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_info);
+    }
+}
